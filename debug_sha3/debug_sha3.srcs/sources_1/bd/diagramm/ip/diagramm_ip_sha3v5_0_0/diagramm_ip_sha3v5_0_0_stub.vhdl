0b11f21d923d1002ac94b2dc9b627fe0bd07a3ba
Anzhelika <anzelikasht@gmail.com>
2019-06-14 15:01:05 +0300

    first commit

---
+++ debug_sha3/debug_sha3.srcs/sources_1/bd/diagramm/ip/diagramm_ip_sha3v5_0_0/diagramm_ip_sha3v5_0_0_stub.vhdl
@@ -0,0 +1,50 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
+-- Date        : Fri Jun 14 14:05:53 2019
+-- Host        : anzThink running 64-bit Ubuntu 16.04.6 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/anzhelika/projekt/diplom/debug_progectv5/debug_sha3/debug_sha3.srcs/sources_1/bd/diagramm/ip/diagramm_ip_sha3v5_0_0/diagramm_ip_sha3v5_0_0_stub.vhdl
+-- Design      : diagramm_ip_sha3v5_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z010clg225-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity diagramm_ip_sha3v5_0_0 is
+  Port ( 
+    s00_axi_awaddr : in STD_LOGIC_VECTOR ( 10 downto 0 );
+    s00_axi_awprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    s00_axi_awvalid : in STD_LOGIC;
+    s00_axi_awready : out STD_LOGIC;
+    s00_axi_wdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    s00_axi_wstrb : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    s00_axi_wvalid : in STD_LOGIC;
+    s00_axi_wready : out STD_LOGIC;
+    s00_axi_bresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s00_axi_bvalid : out STD_LOGIC;
+    s00_axi_bready : in STD_LOGIC;
+    s00_axi_araddr : in STD_LOGIC_VECTOR ( 10 downto 0 );
+    s00_axi_arprot : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    s00_axi_arvalid : in STD_LOGIC;
+    s00_axi_arready : out STD_LOGIC;
+    s00_axi_rdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    s00_axi_rresp : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    s00_axi_rvalid : out STD_LOGIC;
+    s00_axi_rready : in STD_LOGIC;
+    s00_axi_aclk : in STD_LOGIC;
+    s00_axi_aresetn : in STD_LOGIC
+  );
+
+end diagramm_ip_sha3v5_0_0;
+
+architecture stub of diagramm_ip_sha3v5_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "s00_axi_awaddr[10:0],s00_axi_awprot[2:0],s00_axi_awvalid,s00_axi_awready,s00_axi_wdata[31:0],s00_axi_wstrb[3:0],s00_axi_wvalid,s00_axi_wready,s00_axi_bresp[1:0],s00_axi_bvalid,s00_axi_bready,s00_axi_araddr[10:0],s00_axi_arprot[2:0],s00_axi_arvalid,s00_axi_arready,s00_axi_rdata[31:0],s00_axi_rresp[1:0],s00_axi_rvalid,s00_axi_rready,s00_axi_aclk,s00_axi_aresetn";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "ip_sha3v5_v1_0,Vivado 2018.2";
+begin
+end;
